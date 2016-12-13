deaf108531ead8b96aba8c67c167b13d442652ce
Wei WU <drink18@hotmail.com>
2016-12-12 21:19:59 -0500

    mac os build file update

--- CVok2D/cvok2dTestBed/CVok2DTestbed.cpp
+++ CVok2D/cvok2dTestBed/CVok2DTestbed.cpp
@@ -39,10 +39,12 @@ int main(int, char**)
 	if (glfwInit() == 0)
 		return 1;
 
-
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if defined(__APPLE__)
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
 
 	GLFWwindow* window = glfwCreateWindow(1280, 720, "CVok2D", nullptr, nullptr);
 	if (window == NULL)
