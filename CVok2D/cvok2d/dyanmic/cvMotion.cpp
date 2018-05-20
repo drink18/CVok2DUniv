c9e5e42a352036ad7dcd3df8d1952fd1f954bb9c
Wei Wu <wei.wu@MBP-WEIWU.local>
2018-05-20 09:08:20 -0400

    compute inertia correctly

--- CVok2D/cvok2d/dyanmic/cvMotion.cpp
+++ CVok2D/cvok2d/dyanmic/cvMotion.cpp
@@ -1,5 +1,7 @@
 #include "cvMotion.h"
 #include <world/cvBody.h>
+#include <shape/cvCircle.h>
+#include <shape/cvPolygonShape.h>
 
 cvMotionId const cvMotion::StaticMotionId = cvMotionId(0);
 
@@ -15,6 +17,22 @@ void cvMotion::InitializeMotion(cvMotion& motion, cvMotion::MotionType mt, const
     {
         float invMass = 1.0f / cinfo.m_mass;
         float invInertia = 1;
+        if(cinfo.m_shape->getShapeType() == cvShape::eCircle)
+        {
+            auto& circle = static_cast<cvCircle&>(*cinfo.m_shape);
+            float r = circle.getRadius();
+            float inertia = 0.5f * cinfo.m_mass * r * r;
+            invInertia = 1.0f / inertia;
+        }
+        if(cinfo.m_shape->getShapeType() == cvShape::ePolygon)
+        {
+            auto& poly = static_cast<cvPolygonShape&>(*cinfo.m_shape);
+            const cvAabb& aabb = poly.getAabb();
+            float extX = aabb.m_Max.x - aabb.m_Min.x;
+            float extY = aabb.m_Max.y - aabb.m_Min.y;
+            float inertia = 1.0f / 12.0f * cinfo.m_mass * (extX * extX + extY * extY);
+            invInertia = 1.0f / inertia;
+        }
         motion.m_invMassAndInertia.set(invMass, invInertia);
     }
 }
