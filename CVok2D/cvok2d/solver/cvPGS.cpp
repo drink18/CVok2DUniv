e7c79588c25fbb46636f2c7e0aa862944dbfbe47
Wei Wu <wei.wu@MBP-WEIWU.local>
2018-06-05 22:09:59 -0400

    refactor abit

--- CVok2D/cvok2d/solver/cvPGS.cpp
+++ CVok2D/cvok2d/solver/cvPGS.cpp
@@ -136,66 +136,59 @@ void cvPGSSolver::solvePenetrations(bool warmStart)
         auto& c = m_ContactContraints[i];
         cvVec3f velA, velB;
 
-        if(c.bodyAId >= 0)
+        float lambda = c.m_accumImpl;
+
+        if(!warmStart)
         {
-            cvSolverBody& bodyA = m_solverBodies[c.bodyAId];
-            velA = bodyA.m_velocity;
-        }
+            if(c.bodyAId >= 0)
+            {
+                cvSolverBody& bodyA = m_solverBodies[c.bodyAId];
+                velA = bodyA.m_velocity;
+            }
 
-        if(c.bodyBId >= 0)
-        {
-            cvSolverBody& bodyB = m_solverBodies[c.bodyBId];
-            velB = bodyB.m_velocity;
-        }
+            if(c.bodyBId >= 0)
+            {
+                cvSolverBody& bodyB = m_solverBodies[c.bodyBId];
+                velB = bodyB.m_velocity;
+            }
 
-        cvVec3f oldA, oldB;
-        oldA = velA;
-        oldB = velB;
+            cvVec3f oldA, oldB;
+            oldA = velA;
+            oldB = velB;
 
-        // eM
-        float emA = c.JA.dot(c.MA * c.JA);
-        float emB = c.JB.dot(c.MB * c.JB);
-        float em = emA + emB;
+            // eM
+            float emA = c.JA.dot(c.MA * c.JA);
+            float emB = c.JB.dot(c.MB * c.JB);
+            float em = emA + emB;
 
-        // relative vel
-        float v = velA.dot(c.JA) + velB.dot(c.JB);
+            // relative vel
+            float v = velA.dot(c.JA) + velB.dot(c.JB);
 
-        float lambda = -(c.bias + v) / em;
+            lambda = -(c.bias + v) / em;
 
-        if(warmStart)
-        {
-            lambda = c.m_accumImpl;
-            //printf("warm start with %f \n", c.m_accumImpl);
-        }
-
-        else
-        {
             float oldImp = c.m_accumImpl;
             c.m_accumImpl += lambda;
             c.m_accumImpl = std::max(0.0f, c.m_accumImpl);
 
             lambda = c.m_accumImpl - oldImp;
-        }
-
-        velA += c.JA * c.MA * lambda;
-        velB += c.JB * c.MB * lambda;
 
 #if 0
-        const float eps = 1e-5f;
-        if(abs(velA.x - oldA.x) > eps
-                || abs(velB.x - oldB.x) > eps)
-            cvAssertMsg(false, "vel error");
+            const float eps = 1e-5f;
+            if(abs(velA.x - oldA.x) > eps
+                    || abs(velB.x - oldB.x) > eps)
+                cvAssertMsg(false, "vel error");
 
-        float err = velA.dot(c.JA) + velB.dot(c.JB);
-        printf("c%d, re=%f, velA=%f, %f, %f, velB=%f,%f,%f\n", i, err,
-			velA.x, velA.y, velA.z, velB.x, velB.y, velB.z);
+            float err = velA.dot(c.JA) + velB.dot(c.JB);
+            printf("c%d, re=%f, velA=%f, %f, %f, velB=%f,%f,%f\n", i, err,
+                    velA.x, velA.y, velA.z, velB.x, velB.y, velB.z);
 #endif
+        }
 
         if(c.bodyAId >= 0)
-            m_solverBodies[c.bodyAId].m_velocity = velA;
+            m_solverBodies[c.bodyAId].applyVelImp(c.JA, c.MA, lambda);
 
         if(c.bodyBId >= 0)
-            m_solverBodies[c.bodyBId].m_velocity = velB;
+            m_solverBodies[c.bodyBId].applyVelImp(c.JB, c.MB, lambda);
     }
 }
 
@@ -232,14 +225,68 @@ void cvPGSSolver::solvePositionErr()
         float err = velA.dot(c.JA) + velB.dot(c.JB);
 
         if(c.bodyAId >= 0)
-            m_solverBodies[c.bodyAId].m_posVelocity = velA;
+            m_solverBodies[c.bodyAId].applyPosImp(c.JA, c.MA, lambda);
 
         if(c.bodyBId >= 0)
-            m_solverBodies[c.bodyBId].m_posVelocity = velB;
+            m_solverBodies[c.bodyBId].applyPosImp(c.JB, c.MB, lambda);
     }
 }
 
 void cvPGSSolver::solveFriction(bool warmStart)
+{
+    for(auto& c : m_ContactContraints)
+    {
+        cvVec3f velA, velB;
+        float lambda = c.m_tangentImpl;
+
+        if(c.bodyAId >= 0)
+        {
+            cvSolverBody& bodyA = m_solverBodies[c.bodyAId];
+            velA = bodyA.m_velocity;
+        }
+
+        if(c.bodyBId >= 0)
+        {
+            cvSolverBody& bodyB = m_solverBodies[c.bodyBId];
+            velB = bodyB.m_velocity;
+        }
+
+        if(!warmStart)
+        {
+            // eM
+            float emA = c.tJA.dot(c.MA * c.tJA);
+            float emB = c.tJB.dot(c.MB * c.tJB);
+            float em = emA + emB;
+
+
+            // relative vel
+            float v = velA.dot(c.tJA) + velB.dot(c.tJB);
+            lambda = -(c.bias + v) / em;
+
+            float miu = c.m_friction;
+            float oldImp = c.m_tangentImpl;
+            c.m_tangentImpl += lambda;
+            lambda = c.m_tangentImpl;
+            lambda = std::max(-c.m_accumImpl * miu, lambda);
+            lambda = std::min(c.m_accumImpl * miu, lambda);
+            c.m_tangentImpl = lambda;
+            lambda = c.m_tangentImpl - oldImp;
+        }
+
+
+        if(c.bodyAId >= 0)
+        {
+            m_solverBodies[c.bodyAId].applyVelImp(c.tJA, c.MA, lambda);
+        }
+
+        if(c.bodyBId >= 0)
+        {
+            m_solverBodies[c.bodyBId].applyVelImp(c.tJB, c.MB, lambda);
+        }
+    }
+}
+
+void cvPGSSolver::solveRollingFriction()
 {
     for(auto& c : m_ContactContraints)
     {
@@ -265,26 +312,6 @@ void cvPGSSolver::solveFriction(bool warmStart)
 
         // relative vel
         float v = velA.dot(c.tJA) + velB.dot(c.tJB);
-        float lambda = -(c.bias + v) / em;
-
-        if(warmStart)
-        {
-            lambda = c.m_tangentImpl;
-        }
-        else
-        {
-            float miu = c.m_friction;
-            float oldImp = c.m_tangentImpl;
-            c.m_tangentImpl += lambda;
-            lambda = c.m_tangentImpl;
-            lambda = std::max(-c.m_accumImpl * miu, lambda);
-            lambda = std::min(c.m_accumImpl * miu, lambda);
-            c.m_tangentImpl = lambda;
-            lambda = c.m_tangentImpl - oldImp;
-        }
-
-        velA += c.tJA * c.MA * lambda;
-        velB += c.tJB * c.MB * lambda;
 
         // rolling frition
         float rv = velA.dot(c.rJA) + velB.dot(c.rJB);
@@ -294,12 +321,15 @@ void cvPGSSolver::solveFriction(bool warmStart)
         velA += c.rJA * c.MA * rlambda;
         velB += c.rJB * c.MB * rlambda;
 
-
         if(c.bodyAId >= 0)
-            m_solverBodies[c.bodyAId].m_velocity = velA;
+        {
+            m_solverBodies[c.bodyAId].applyVelImp(c.rJA, c.MA, rlambda);
+        }
 
         if(c.bodyBId >= 0)
-            m_solverBodies[c.bodyBId].m_velocity = velB;
+        {
+            m_solverBodies[c.bodyBId].applyVelImp(c.rJB, c.MB, rlambda);
+        }
     }
 }
 
@@ -308,18 +338,23 @@ void cvPGSSolver::solveContacts(int nIter)
     if(m_ContactContraints.size() == 0)
         return;
 
+    // warm start
     solveFriction(true);
     solvePenetrations(true);
 
 #if 1
     for(int i = 0; i < nIter; ++i)
         solveFriction(false);
+
 #endif
 
     //printf("==================BEGIN==============\n");
     for(int i = 0; i < nIter; ++i)
         solvePenetrations(false);
     //printf("==================END==============\n");
+
+    for(int i = 0; i < nIter; ++i)
+        solveRollingFriction();
 
     for(int i = 0; i < nIter; ++i)
         solvePositionErr();
